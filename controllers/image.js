912a06f80042cce5b898fecddac65ede23efead9
Mangalnathan Vijayagopal <mangalnathanvg@gmail.com>
2020-08-03 18:50:43 -0400

    First Commit

---
+++ controllers/image.js
@@ -0,0 +1,27 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '9003dfe10f6a48298d2e818478e3d8be'
+});
+ 
+const handleApiCall = (req, res) => {
+app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+.then(data => {
+    res.json(data);
+})
+.catch(err => res.status(400).json('Unable to work with API'))
+}
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id).increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    }).catch(err => res.status(400).json('Unable to get entries'))
+}
+
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
